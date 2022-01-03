1c1c47e9b512bd54d0e671b2f9397822b28958b4
Stefan Heid <stefan.heid@upb.de>
2022-01-03 16:58:16 +0100

    added logging and dockerhub integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,13 +11,22 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+application {
+    mainClass.set("ApplicationKt")
+}
 
 dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("io.ktor:ktor-server-core:1.6.7")
+    implementation("io.ktor:ktor-server-netty:1.6.7")
     implementation("com.sksamuel.hoplite:hoplite-core:1.4.16")
     implementation("com.sksamuel.hoplite:hoplite-yaml:1.4.16")
     implementation("com.github.kittinunf.fuel:fuel:2.3.1")
     implementation("org.jsoup:jsoup:1.14.3")
     implementation("com.google.code.gson:gson:2.8.9")
+
+    implementation("io.github.microutils:kotlin-logging-jvm:2.1.0")
+    implementation("org.slf4j:slf4j-log4j12:1.7.32")
     testImplementation(kotlin("test"))
 }
 
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,10 +1,19 @@
 import com.sksamuel.hoplite.ConfigLoader
 import java.time.LocalDate
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import java.lang.RuntimeException
 
 
-fun main() {
+fun getUnhelpfulFoodsavers(): String {
+    val user: String = System.getenv("USER") ?: throw RuntimeException("USER not provided")
+    val pw: String = System.getenv("PWD") ?: throw RuntimeException("PWD not provided")
+
+    val login = Credentials(user, pw)
     val cfg = ConfigLoader().loadConfigOrThrow<Config>("/application.yaml")
-    val login = ConfigLoader().loadConfigOrThrow<Credentials>("/credencials.yaml")
 
     val fairteiler = cfg.stores.fairteilers
     val sess = Session(login, cfg.server)
@@ -22,7 +31,19 @@ fun main() {
                 .map { (store, entry) -> "${store.name} ${entry.single { (saver, _) -> saver == leecher }.second}" }
         }
 
-    leechers.forEach { println("${it.key} -> ${it.value}") }
+    return leechers.map { "${it.key} -> ${it.value}" }.joinToString("\n")
+}
+
+fun main() {
+    val path: String = System.getenv("SECRET") ?: throw RuntimeException("SECRET not provided")
+    embeddedServer(Netty, port = 80) {
+        routing {
+            get("/${path}/check") {
+                log.info("responding to request /${path}")
+                call.respondText(getUnhelpfulFoodsavers())
+            }
+        }
+    }.start(wait = true)
 }
 
 private fun <K, V> Map<K, Collection<Pair<Saver, V>>>.toSetOfSavers(): Set<Saver> {
