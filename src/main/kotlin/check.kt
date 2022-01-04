e4b1b041ac0e3848b77db1df0f87ff1ea6d2e7ee
Stefan Heid <stefan.heid@upb.de>
2022-01-04 15:58:36 +0100

    fixed minor error with inactive filtering

--- src/main/kotlin/check.kt
+++ src/main/kotlin/check.kt
@@ -13,7 +13,7 @@ fun getUnhelpfulFoodsavers(): String {
     val sess = Session(login, cfg.server)
     val today = LocalDate.now()
     val inactiveDate = today.minusMonths(2)
-    val observationWindow = today.minusMonths(2)
+    val observationWindow = today.minusMonths(6)
 
     fun Store.isFairteiler(): Boolean {
         return fairteiler.contains(this.id)
@@ -23,13 +23,19 @@ fun getUnhelpfulFoodsavers(): String {
         sess.getSaversIn(store).filter { (_, lastDate) -> lastDate?.let { it > observationWindow } == true }
     }
 
-    val saversActiveInStores = stores.filter { !it.key.isFairteiler() }.getSavers { it < inactiveDate }
+    val saversActiveInStores = stores.filter { !it.key.isFairteiler() }.getSavers { it > inactiveDate }
     val saversInFairteilers = stores.filter { it.key.isFairteiler() }.getSavers()
 
+    // mapping of foodsavers to list of his latest pickups per store
     val criticalIndividuals = (saversActiveInStores - saversInFairteilers)
-        .associateWith { leecher ->
-            stores.filter { (store, savers) -> savers.map { (saver, _) -> saver }.contains(leecher) }
-                .map { (store, entry) -> "${store.name} ${entry.single { (saver, _) -> saver == leecher }.second}" }
+        .associateWith { saver ->
+            stores
+                // stores that contain the saver
+                .filter { (_, savers) -> savers.any { (saver_, _) -> saver_ == saver } }
+                // as a list of (store, date), where date is the date, this saver did the last pickup
+                .map { (store, entry) -> store.name to entry.single { (saver_, _) -> saver_ == saver }.second }
+                .sortedByDescending { (_, lastDate) -> lastDate }
+                .map { "${it.first} ${it.second}" }
         }
 
     return criticalIndividuals.toSortedMap(compareBy { it.name }).map { "${it.key} -> ${it.value}" }.joinToString("\n")
